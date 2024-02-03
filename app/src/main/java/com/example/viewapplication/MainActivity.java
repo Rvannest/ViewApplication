c869d7fc0657c97297869d6ed41f7d9f722ffdd6
Robert <vannestr@uwindsor.ca>
2024-02-03 12:33:19 -0500

    Q2 done, reset button appears and disappears, removed toast message with an alert dialog

--- app/src/main/java/com/example/viewapplication/MainActivity.java
+++ app/src/main/java/com/example/viewapplication/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.viewapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,21 +29,37 @@ public class MainActivity extends AppCompatActivity {
         btn_down = findViewById(R.id.button_down);
         editText = findViewById(R.id.editTextText);
 
+        btn_reset.setVisibility(View.GONE);
+
         btn_count.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 counter = counter+1;
                 textView_number.setText(String.valueOf(counter));
+                btn_reset.setVisibility(View.VISIBLE);
             }
         });
 
         btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                counter = 0;
-                textView_number.setText(String.valueOf(counter));
-                textView_number.setBackgroundColor(Color.BLACK);
-                btn_count.setTextColor(Color.RED);
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Alert!")
+                        .setMessage("Do you want to reset?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                counter = 0;
+                                textView_number.setText(String.valueOf(counter));
+                                //textView_number.setBackgroundColor(Color.BLACK);
+                                //btn_count.setTextColor(Color.RED);
+                                //Toast.makeText(MainActivity.this, "Counter reset", Toast.LENGTH_SHORT).show();
+                                btn_reset.setVisibility(View.GONE);
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
             }
         });
 
